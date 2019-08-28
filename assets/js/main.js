f5fc8d3cbde329f62ab134c84de1cdee0fd300bc
unknown <shaonkabir98@gmail.com>
2019-08-27 23:48:44 -0700

    Tinyone Template Design

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+	$(document).ready(function() {
+
+
+		// Homepage Slider Carousel
+
+		$('.homepage-slides').owlCarousel({
+			items:1,
+			autoplay:true,
+			dots:true,
+			loop:true,
+			nav:false
+		});
+	
+	});
+
+
+	
